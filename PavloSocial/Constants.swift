1092145590633391e5fdc34fc572c8c91d839d43
Pavlos Nicolaou <pauln1993@gmail.com>
2016-08-26 13:15:05 +0300

    Customised sign in UI

---
+++ PavloSocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  PavloSocial
+//
+//  Created by Pavlos Nicolaou on 26/08/2016.
+//  Copyright © 2016 Pavlos Nicolaou. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ PavloSocial/FancyBtn.swift
@@ -0,0 +1,25 @@
+//
+//  FancyBtn.swift
+//  PavloSocial
+//
+//  Created by Pavlos Nicolaou on 26/08/2016.
+//  Copyright © 2016 Pavlos Nicolaou. All rights reserved.
+//
+
+import UIKit
+
+class FancyBtn: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 6.0
+        
+    }
+    
+
+}
