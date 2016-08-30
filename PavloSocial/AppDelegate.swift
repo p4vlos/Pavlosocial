6fdd41f6403f097a8bb559d0011ee4509d6bd29a
Pavlos Nicolaou <vl1s@Pavloss-MacBook-Pro.local>
2016-08-30 14:23:21 +0300

    trying to fix facebook auth errors

--- PavloSocial/AppDelegate.swift
+++ PavloSocial/AppDelegate.swift
@@ -48,7 +48,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: AnyObject) -> Bool {
         return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 }
