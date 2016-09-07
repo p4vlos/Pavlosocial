1bcfbc3afca286d73fd7c120bbfc49e7d4e48cb0
Pavlos Nicolaou <vl1s@Pavloss-MacBook-Pro.local>
2016-09-07 04:47:01 +0300

    Completed Firebase get

--- PavloSocial/FeedVC.swift
+++ PavloSocial/FeedVC.swift
@@ -12,6 +12,9 @@ import SwiftKeychainWrapper
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,7 +22,20 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
+            
+            self.posts = [] // THIS IS THE NEW LINE
+            
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
         
     }
@@ -29,10 +45,13 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        print("JESS: \(post.caption)")
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
---
+++ PavloSocial/Post.swift
@@ -0,0 +1,57 @@
+//
+//  Post.swift
+//  PavloSocial
+//
+//  Created by Pavlos Nicolaou on 07/09/2016.
+//  Copyright © 2016 Pavlos Nicolaou. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    private var _caption: String!
+    private var _imageUrl: String!
+    private var _likes: Int!
+    private var _postKey: String!
+    
+    var caption: String {
+        return _caption
+    }
+    
+    var imageUrl: String {
+        return _imageUrl
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var postKey: String {
+        return _postKey
+    }
+    
+    init(caption: String, imageUrl: String, likes: Int) {
+        self._caption = caption
+        self._likes = likes
+        self._imageUrl = imageUrl
+    }
+    
+    init(postKey: String, postData: Dictionary<String, AnyObject>) {
+        self._postKey = postKey
+        
+        if let caption = postData["caption"] as? String {
+            self._caption = caption
+        }
+        
+        if let imageUrl = postData["imageUrl"] as? String {
+            self._imageUrl = imageUrl
+        }
+        
+        if let likes = postData["likes"] as? Int {
+            self._likes = likes
+        }
+        
+        //_postRef = DataService.ds.REF_POSTS.child(_postKey)
+
+    }
+}
