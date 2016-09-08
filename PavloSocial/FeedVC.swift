20355233f0349647c093e750f76cadb7480df7a3
p4vlos <nicolaou_paul@hotmail.com>
2016-09-08 18:25:34 +0300

    Hello Pavlos

--- PavloSocial/FeedVC.swift
+++ PavloSocial/FeedVC.swift
@@ -93,7 +93,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
             return
         }
         //guard let img = imageAdd.image else {
-        guard let img = imageAdd.image, imageSelected == true else {
+        //guard let img = imageAdd.image, imageSelected == true else {
             print("PAV: An image must be selected")
             return
         }
@@ -108,7 +108,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
                 if error != nil {
                     print("PAV: Unable to upload image to Firebasee torage")
                 } else {
-                    //print("PAVs: Successfully uploaded image to Firebase storage")
+                    print("PAV: Successfully uploaded image to Firebase storage")
                     _ = metadata?.downloadURL()?.absoluteString
                 }
             }
